546d1575771ed09c26b9059f111277491d8da791
Alberto <aldo10012@gmail.com>
2020-09-03 17:23:27 -0700

    finished tutorial
    finished tutorial

--- functions.py
+++ functions.py
@@ -3,3 +3,21 @@ def area(width, height):
     return result
 result = area(5, 6)
 print(result)
+
+print(are(9,3))
+print(area(2,7))
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+print(subtract(7,1))
+print(subtract(13,5))
+
+def divide(num1, num2)
+    result = num1 / num2
+    return result
+
+def add(num1, num2)
+    result = num1 + num2
+    return result
